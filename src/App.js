f2547a9a88faa89607a53ef34783eeb5f773014d
Nathan Hietala <Nrhietala@gmail.com>
2020-12-11 10:21:58 -0600

    Basic structure set up, data coming in, starting Header component and working down

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import Header from './components/Header';
+// import Gallery from './components/Gallery';
+// import Footer from './components/Footer';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  const [ charData, setCharData ] = useState([])
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  useEffect(()=> {
+    axios
+    .get("https://rickandmortyapi.com/api/character")
+    .then(res => {
+      setCharData(res.data.results);
+    })
+    .catch(err => {
+      console.log(err)
+    }) 
+  },[])
+
+  /********************
+  App
+  Header
+  Gallery -> Character
+  Footer
+  *********************/
 
   return (
     <div className="App">
-      <h1 className="Header">I'm working :)</h1>
+      <Header/>
     </div>
   );
 }
